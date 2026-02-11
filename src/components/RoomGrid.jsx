73365e6c285c8671ed6b301f5fbd64c1f5041c7f
Vishwas-gour <vishwasgour2002@gmail.com>
2026-02-11 16:21:23 +0530

    saved or not saved room icon in room pages

--- src/components/RoomGrid.jsx
+++ src/components/RoomGrid.jsx
@@ -1,94 +1,164 @@
-import React from 'react'
+
+import React, { useState, useEffect } from "react";
+import { useWishlist } from "../context/WishlistContext";
+
 import Api from '../api/Api';
 import "../css/room-greed.css"
-import { Trash2 } from "lucide-react"
 // useLocation add kiya
-import { MapPin, ArrowRight } from "lucide-react";
+import { MapPin, ArrowRight, Trash2, Heart, HeartOff } from "lucide-react";
 import { useNavigate } from 'react-router-dom';
 
 
 function RoomGrid({ rooms, applyFilters }) {
-    const role = localStorage.getItem("role");
-    const navTo = useNavigate();
-    const handleDelete = async (roomId) => {
-        if (!window.confirm("Delete this room permanently?")) return;
+  const [wishlistRoomIds, setWishlistRoomIds] = useState([]);
 
-        try {
-            await Api.delete(`/admin/rooms/${roomId}`);
-            alert("Room deleted successfully");
-            applyFilters(); // reload list
-        } catch (err) {
-            console.error(err);
-            alert("Failed to delete room");
-        }
-    };
+  const role = localStorage.getItem("role");
+  const navTo = useNavigate();
+  const { fetchCount } = useWishlist();
 
-    if (role == "ROLE_ADMIN") return (
-        <div className="admin-rooms-grid">
-            {rooms.length === 0 && <p>No rooms found</p>}
+  const handleDelete = async (roomId) => {
+    if (!window.confirm("Delete this room permanently?")) return;
 
-            {rooms.map((room) => (
-                <div key={room.id} className="admin-room-card">
-                    <img
-                        src={room.imageUrls?.[0] || "/placeholder.jpg"}
-                        alt="room"
-                        onClick={() => navTo(`/room/${room.id}`)}
-                    />
+    try {
+      await Api.delete(`/admin/rooms/${roomId}`);
+      alert("Room deleted successfully");
+      applyFilters(); // reload list
+    } catch (err) {
+      console.error(err);
+      alert("Failed to delete room");
+    }
+  };
+  const isInWishlist = (roomId) => wishlistRoomIds.includes(roomId);
 
-                    <div className="details">
-                        <h4>{room.title}</h4>
-                        <p>City: {room.city}</p>
-                        <p>Owner: {room.ownerEmail}</p>
+  const fetchWishlistIds = async () => {
+    const email = localStorage.getItem("email");
+    console.log("Asdf")
+    if (!email) return;
 
-                        <span className={`status ${room.isApprovedByAdmin ? "approved" : "pending"}`}>
-                            {room.isApprovedByAdmin ? "Approved" : "Pending"}
-                        </span>
-                    </div>
+    Api.get(`/wishlist?email=${email}`)
+      .then((res) => {
+        console.log(res.data)
+        setWishlistRoomIds(res.data.map(r => r.room.id))
+      })
+      .catch((err) => console.error("Wishlist load failed", err));
+    // assuming res.data = [{id: 1}, {id: 5}]
 
-                    <button
-                        className="del-btn"
-                        onClick={() => handleDelete(room.id)}
-                    >
-                        <Trash2 size={18} />
-                    </button>
-                </div>
-            ))}
+  };
+
+  useEffect(() => {
+    fetchWishlistIds()
+  }, [])
+
+  const handleAddToWishlist = async (e, roomId) => {
+    e.stopPropagation();
+    const email = localStorage.getItem("email");
+
+    if (!email) {
+      alert("Please login first");
+      return;
+    }
+
+    try {
+      if (wishlistRoomIds.includes(roomId)) {
+        await Api.delete(`/wishlist/${roomId}`, { params: { email } });
+        setWishlistRoomIds(prev => prev.filter(id => id !== roomId));
+        alert("room remove")
+      } else {
+        await Api.post(`/wishlist/${roomId}`, null, { params: { email } });
+        setWishlistRoomIds(prev => [...prev, roomId]);
+        alert("room Saved")
+      }
+
+      fetchCount(); // refresh navbar count
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  if (role == "ROLE_ADMIN") return (
+    <div className="admin-rooms-grid">
+      {rooms.length === 0 && <p>No rooms found</p>}
+
+      {rooms.map((room) => (
+        <div key={room.id} className="admin-room-card">
+          <img
+            src={room.imageUrls?.[0] || "/placeholder.jpg"}
+            alt="room"
+            onClick={() => navTo(`/room/${room.id}`)}
+          />
+
+          <div className="details">
+            <h4>{room.title}</h4>
+            <p>City: {room.city}</p>
+            <p>Owner: {room.ownerEmail}</p>
+
+            <span className={`status ${room.isApprovedByAdmin ? "approved" : "pending"}`}>
+              {room.isApprovedByAdmin ? "Approved" : "Pending"}
+            </span>
+          </div>
+
+          <button
+            className="del-btn"
+            onClick={() => handleDelete(room.id)}
+          >
+            <Trash2 size={18} />
+          </button>
         </div>
-    )
-    else return (
-         <div className="rooms-grid">
-        {rooms.length > 0
-          ? rooms.map((room) => (
-              <div
-                key={room.id}
-                className="room-card"
-                onClick={() => navTo(`/room/${room.id}`)}
-              >
-                <img
-                  src={room.imageUrls?.[0] || "/placeholder.jpg"}
-                  alt={room.title}
-                />
+      ))}
+    </div>
+  )
+  else return (
+    <div className="rooms-grid">
+      {rooms.length > 0
+        ? rooms.map((room) => (
+          <div
+            key={room.id}
+            className="room-card"
+            onClick={() => navTo(`/room/${room.id}`)}
+          >
+            <img
+              src={room.imageUrls?.[0] || "/placeholder.jpg"}
+              alt={room.title}
+            />
 
-                <div className="card-content">
-                  <h3>{room.title}</h3>
-                  <p>
-                    <MapPin size={14} /> {room.city}
-                  </p>
-                  <div className="price-type-row">
-                    <span className="price">₹{room.price}</span>
-                    <span className="room-type-tag">{room.roomType}</span>
-                  </div>
-                  <span className="view-link">
-                    View Details <ArrowRight size={14} />
-                  </span>
-                </div>
+            <button
+              className="wishlist-btn"
+              onClick={(e) => handleAddToWishlist(e, room.id)}
+            >
+              {isInWishlist(room.id) ? (
+                <HeartOff size={18} />
+              ) : (
+                <Heart size={18} />
+              )}
+            </button>
+
+
+
+            <div className="card-content">
+              <h3>{room.title}</h3>
+
+              <p>
+                <MapPin size={14} /> {room.city}
+              </p>
+
+              <div className="price-type-row">
+                <span className="price">₹{room.price}</span>
+                <span className="room-type-tag">{room.roomType}</span>
               </div>
-            ))
-          :  (
-              <div className="no-rooms">No rooms found for this category.</div>
-            )}
-      </div>
-    )
+
+              <span className="view-link">
+                View Details <ArrowRight size={14} />
+              </span>
+            </div>
+          </div>
+
+        ))
+        : (
+          <div className="no-rooms">No rooms found for this category.</div>
+        )}
+    </div>
+  )
 }
 
 export default RoomGrid
