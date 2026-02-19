faaf647ef644d1cab603aac3ec7b4fded1da88ef
nikhil0441 <nikhilpatel03022004@gmail.com>
2026-02-19 22:25:43 +0530

    Update API base URL to production backend

--- src/data/env.js
+++ src/data/env.js
@@ -1,8 +1,9 @@
-export const googleClientId ="1059255546776-6fiqjph21l9v7e7ab0uui6k0u1ocudo4.apps.googleusercontent.com";
-export const razorPayKeyId = "rzp_test_1DP5mmOlF5G5ag";
 
 const isMobile = /Android|iPhone|iPad/i.test(navigator.userAgent);
 
-export const API_BASE = isMobile? "http://192.168.1.6:8080/api": "http://localhost:8080/api";
-// export const API_BASE =  "http://localhost:8080/api";
+export const googleClientId =
+  "1059255546776-6fiqjph21l9v7e7ab0uui6k0u1ocudo4.apps.googleusercontent.com";
 
+export const razorPayKeyId = "rzp_test_1DP5mmOlF5G5ag";
+
+export const API_BASE = "https://api.roomsdekho.in/api";
