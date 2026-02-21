71e9fec3e34a701d0f0419bab55b0d274be48ff8
nikhil0441 <nikhilpatel03022004@gmail.com>
2026-02-21 20:43:35 +0530

    Added

--- src/data/env.js
+++ src/data/env.js
@@ -6,5 +6,6 @@ export const googleClientId = "1059255546776-6fiqjph21l9v7e7ab0uui6k0u1ocudo4.ap
 export const razorPayKeyId = "rzp_test_1DP5mmOlF5G5ag";
 
 const isLocalhost = window.location.hostname === "localhost"
+const A="http://localhost:8080/api";
 
 export const API_BASE = isLocalhost ? "http://localhost:8080/api" : "https://api.roomsdekho.in/api";
