ee07b91aad79053f7c12d8fe4898bb8e7dec9a77
nikhil0441 <nikhilpatel03022004@gmail.com>
2026-05-26 12:03:56 +0530

    fixed https backend

--- src/data/env.js
+++ src/data/env.js
@@ -1,10 +1,12 @@
-
 const isMobile = /Android|iPhone|iPad/i.test(navigator.userAgent);
 
-export const googleClientId = "1059255546776-6fiqjph21l9v7e7ab0uui6k0u1ocudo4.apps.googleusercontent.com";
+export const googleClientId =
+  "1059255546776-6fiqjph21l9v7e7ab0uui6k0u1ocudo4.apps.googleusercontent.com";
 
 export const razorPayKeyId = "rzp_test_1DP5mmOlF5G5ag";
 
-const isLocalhost = window.location.hostname === "localhost"
+const isLocalhost = window.location.hostname === "localhost";
 const A = "http://52.66.93.245:8080/api";
-export const API_BASE = isLocalhost ? "http://localhost:8080/api" : "http://52.66.93.245:8080/api";
+export const API_BASE = isLocalhost
+  ? "http://localhost:8080/api"
+  : "https://api.roomsdekho.in/api";
